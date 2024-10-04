c67798ad826db230a457263798019aa006e20412
김대호 <kimdaeho@gimdaehoui-MacBookPro.local>
2024-10-04 18:11:08 +0900

    first commit

---
+++ src/main/java/com/autocrypt/safeno/SafenoApplication.java
@@ -0,0 +1,13 @@
+package com.autocrypt.safeno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafenoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafenoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autocrypt/safeno/SafenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autocrypt.safeno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafenoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
